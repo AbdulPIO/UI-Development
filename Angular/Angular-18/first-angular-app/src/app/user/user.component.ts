32f1c14c4bc06f59e20fbd6936dc052bc5ba04ae
AbdulPIO <abdul.khan@programmers.io>
2025-03-10 17:50:12 +0530

    Angular Input & Output Decorators, If and For, Input and Signal Inputs

--- Angular/Angular-18/first-angular-app/src/app/user/user.component.ts
+++ Angular/Angular-18/first-angular-app/src/app/user/user.component.ts
@@ -1,9 +1,23 @@
-import { Component, signal, computed, Input } from '@angular/core';
+import { Component, signal, computed, Input, input, Output, EventEmitter, output } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 // Helper for getting random user
 
 // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+
+// Using a type alias to outsource large objects when used in @Input
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// };
+
+// Using interface in place of type alias
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-user',
@@ -12,14 +26,34 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input() avatar!: string;
-  @Input() name!: string;
+  // Using Input decorator
+  // @Input({required: true}) id!: string;
+  // @Input({required: true}) avatar!: string;
+  // @Input({required: true}) name!: string;
 
-  get imagePath() {
-    return 'assets/users/' + this.avatar;
+  // Instead of accepting one property as above we can accept the whole object
+  @Input({required: true}) user!:  User;
+  // adding custom event using Output decorator
+  @Output() select = new EventEmitter();
+
+  // Using the output() function
+  // select = output<string>();
+
+  get imagePath () {
+    return 'assets/users/' + this.user.avatar;
   }
 
-  onSelectUser() {}
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+
+
+  // Using Signal input
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  // imagePath = computed(() =>{return 'assets/users/' + this.avatar()});
+
   // Using Zone For State Change
   // selectedUser = DUMMY_USERS[randomIndex]
 
