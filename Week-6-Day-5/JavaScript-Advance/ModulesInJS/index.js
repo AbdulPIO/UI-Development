9b157fd43fc7afb37ed1cb33235daa93df095461
AbdulPIO <abdul.khan@programmers.io>
2025-03-21 13:15:26 +0530

    Delete Week-6-Day-5 directory

--- Week-6-Day-5/JavaScript-Advance/ModulesInJS/index.js
+++
@@ -1,8 +0,0 @@
-// import { Teacher } from "./teacher.js";
-// import { promote } from "./teacher.js"
-
-// since Teacher is the default export we don't need curly braces
-import Teacher, { promote } from "./teacher.js";
-
-const teacher = new Teacher("Ms. Ann", "MSc");
-teacher.teach();
