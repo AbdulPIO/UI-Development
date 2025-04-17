e8010a6f946886cab342c3107cb4e5e9089a167b
AbdulPIO <abdul.khan@programmers.io>
2025-04-17 16:26:54 +0530

    ViewEncapsulation, ViewChild, ViewContent, Component Lifecycle in Angular 16

--- Angular/Angular-16/componentsdatabinding-angular-app/src/app/cockpit/cockpit.component.ts
+++ Angular/Angular-16/componentsdatabinding-angular-app/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -9,20 +9,25 @@ export class CockpitComponent {
   // properties set as event that we can emit
   @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
-  newServerName = '';
-  newServerContent = '';
+  // newServerName = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
 
-  onAddServer() {
+  onAddServer(nameInput: HTMLInputElement) {
+    // console.log(nameInput.value);
+
+    console.log(this.serverContentInput);
+
     this.serverCreated.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
     })
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
     })
   }
 }
--- Angular/Angular-16/componentsdatabinding-angular-app/src/app/server-element/server-element.component.ts
+++ Angular/Angular-16/componentsdatabinding-angular-app/src/app/server-element/server-element.component.ts
@@ -1,10 +1,44 @@
-import { Component, Input } from '@angular/core';
+import { AfterContentInit, Component, ContentChild, DoCheck, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewChild, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated // None / ShadowDOM
 })
-export class ServerElementComponent {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, OnDestroy {
   @Input('srvElement') element: { type: string, name: string, content: string }
+  @Input() name: string;
+  @ViewChild('heading') header: ElementRef;
+  @ContentChild('contentParagraph') paragraph: ElementRef;
+
+  // constructor is called first, before ngOnInit
+  constructor() { // execute everytime an instance of this component is created
+    console.log('constructor called!');
+  }
+
+  ngOnChanges(changes: SimpleChanges) { // called before ngOnInit
+    console.log('ngOnChanges called!');
+    console.log(changes); // shows thebound input property i.e., element in this case
+
+  }
+
+  ngOnInit() { // execute everytime an instance of this component is created
+    console.log('ngOnInit called!');
+  }
+
+  ngDoCheck() { // runs on evry change detection run
+    console.log('ngDoCheck called!');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit called!');
+    console.log('Text Content: ' + this.paragraph.nativeElement.textContent);
+
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy called!');
+
+  }
 }
