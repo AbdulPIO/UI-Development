6cbdcdce02a9f375376b5112938b6d68663c3f6f
AbdulPIO <abdul.khan@programmers.io>
2025-05-20 10:21:06 +0530

    Removed unused imports

--- Angular/Angular-16/authentication-angular-app/src/app/auth.guard.ts
+++ Angular/Angular-16/authentication-angular-app/src/app/auth.guard.ts
@@ -1,7 +1,6 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { CanActivate, Router } from "@angular/router";
 import { AuthService } from "./auth.service";
-import { Observable } from "rxjs";
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
