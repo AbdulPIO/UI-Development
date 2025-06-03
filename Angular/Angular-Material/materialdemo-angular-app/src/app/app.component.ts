a9ea5e780f5f62ec2e3db0e2950546db6df46464
AbdulPIO <abdul.khan@programmers.io>
2025-06-03 16:51:17 +0530

    Angular Material, button, toggle, spinner, navbar, sidenav

---
+++ Angular/Angular-Material/materialdemo-angular-app/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { MaterialModule } from './material/material.module';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [MaterialModule, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'materialdemo-angular-app';
+  notificaions = 2;
+
+  opened = false;
+}
